a26906638b935f84be534e2a798d5702e65e9fdc
oscarsanchez-forgeone <oscardelavega920@gmail.com>
2026-08-03 14:53:28 -0500

    oscar assistant v0.2 fase 1

--- sw.js
+++ sw.js
@@ -1,4 +1,1 @@
-const CACHE='oscar-assistant-v1';
-const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='oscar-assistant-v020';const ASSETS=['./','index.html','manifest.json','icon-192.png','icon-512.png'];self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))))
